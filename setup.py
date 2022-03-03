b76e69e0d02f4d983b14d36713f0e107723fe1d3
Felix Schön <mail@fschoen.com>
2022-03-03 15:05:10 +0100

    Setup initial framework

---
+++ setup.py
@@ -0,0 +1,23 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+# Load long description
+LOCATION = path.abspath(path.dirname(__file__))
+with open(path.join(LOCATION, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="sCoda",
+    packages=find_packages(include=["sCoda"]),
+    version="0.1",
+    description="A music library for judging difficulty of pieces",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Felix Schoen",
+    license="MIT",
+    install_requires=["mido >= 1.2.10"],
+    setup_requires=["pytest-runner >= 6.0.0", "twine >= 3.8.0"],
+    tests_require=["pytest >= 7.0.1"],
+    test_suite="test",
+)
