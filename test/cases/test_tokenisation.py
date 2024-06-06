a21dd5baf4ed09a3603314ff3a4744596b903c62
Felix Schön <mail@fschoen.com>
2024-06-06 22:44:16 +0200

    Release v2.1-beta.8

--- test/cases/test_tokenisation.py
+++ test/cases/test_tokenisation.py
@@ -102,9 +102,6 @@ def _test_roundtrip_tokenisation(tokeniser, path_resource, quantise=True, detoke
     if quantise:
         sequence.quantise_and_normalise()
 
-    sequence.save("original.mid")
-    print(sequence.abs.messages)
-
     bars = Sequence.sequences_split_bars([sequence], 0)[0]
 
     if quantise:
@@ -125,9 +122,7 @@ def _test_roundtrip_tokenisation(tokeniser, path_resource, quantise=True, detoke
 
     sequence_roundtrip = tokeniser.detokenise(tokens)
 
-    sequence_roundtrip.save("roundtrip.mid")
-    sequence.save("original.mid")
-    # assert sequence == sequence_roundtrip
+    assert sequence == sequence_roundtrip
 
     return tokens, sequence_roundtrip
 
@@ -141,11 +136,11 @@ def _test_constraints(tokeniser, tokens):
         valid_tokens, previous_state = tokeniser.get_constraints([tokens[i]], previous_state)
 
 
-def test_single():
-    tokeniser = RelativeNotelikeTokeniser(running_value=True, running_time_sig=True)
-
-    tokens, sequence_roundtrip = _test_roundtrip_tokenisation(tokeniser, RESOURCES_ROOT.joinpath("subject.mid"))
-
-    sequence_roundtrip.save("roundtrip.mid")
-    print(tokens)
-    print(sequence_roundtrip.abs.messages)
+# def test_single():
+#     tokeniser = RelativeNotelikeTokeniser(running_value=True, running_time_sig=True)
+#
+#     tokens, sequence_roundtrip = _test_roundtrip_tokenisation(tokeniser, RESOURCES_ROOT.joinpath("subject.mid"))
+#
+#     sequence_roundtrip.save("roundtrip.mid")
+#     print(tokens)
+#     print(sequence_roundtrip.abs.messages)
