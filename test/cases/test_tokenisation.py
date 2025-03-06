c12f19666c9b5f0dfd5720dc2b35b3bdad085105
Felix Schön <mail@fschoen.com>
2025-03-06 21:06:31 +0100

    Remove tokenisation remnants

--- test/cases/test_tokenisation.py
+++ test/cases/test_tokenisation.py
@@ -1,6 +1,5 @@
 from base import *
-from scoda.tokenisation.notelike_tokenisation import MultiTrackLargeVocabularyNotelikeTokeniser, \
-    LargeVocabularyNotelikeTokeniser
+from scoda.tokenisation.notelike_tokenisation import MultiTrackLargeVocabularyNotelikeTokeniser
 
 
 @pytest.mark.parametrize("path_resource", [RESOURCE_MULTI_TRACK])
@@ -8,7 +7,7 @@ def test_roundtrip_multi_track_large_vocabulary_notelike_tokeniser(path_resource
     tokeniser = MultiTrackLargeVocabularyNotelikeTokeniser(num_instruments=4)
 
     tokens, sequences_original, sequences_roundtrip = _test_roundtrip_multi_track_tokenisation(tokeniser, path_resource,
-                                                                                             quantise=True)
+                                                                                               quantise=True)
 
     info = tokeniser.get_info(tokens)
 
@@ -91,61 +90,11 @@ def _test_roundtrip_multi_track_tokenisation(tokeniser, path_resource, quantise=
         roundtrip_bars = Sequence.sequences_split_bars(sequences_roundtrip, 0)
         for c, (channel_original, channel_roundtrip) in enumerate(zip(sequences_bars, roundtrip_bars)):
             for b, (bar_original, bar_roundtrip) in enumerate(zip(channel_original, channel_roundtrip)):
-                assert bar_original.sequence.equivalent(bar_roundtrip.sequence, ignore_channel=True, ignore_velocity=True)
+                assert bar_original.sequence.equivalent(bar_roundtrip.sequence, ignore_channel=True,
+                                                        ignore_velocity=True)
 
     # Check equivalence of output sequence
     for sequence_original, sequence_roundtrip in zip(sequences_original, sequences_roundtrip):
         assert sequence_original.equivalent(sequence_roundtrip, ignore_channel=True, ignore_velocity=True)
 
     return tokens, sequences_original, sequences_roundtrip
-
-
-# OLD
-
-@pytest.mark.parametrize("path_resource", RESOURCES)
-@pytest.mark.parametrize("running_time_sig", [True, False])
-def test_roundtrip_large_vocabulary_notelike_tokenisation(path_resource, running_time_sig):
-    tokeniser = LargeVocabularyNotelikeTokeniser(running_time_sig=running_time_sig)
-
-    _test_roundtrip_tokenisation(tokeniser, path_resource, quantise=True)
-
-
-def _test_roundtrip_tokenisation(tokeniser, path_resource, quantise=True, detokenise=True):
-    sequences = Sequence.sequences_load(file_path=path_resource)
-    sequence = sequences[0]
-    sequence.merge(sequences[1:])
-
-    if quantise:
-        sequence.quantise_and_normalise()
-
-    bars = Sequence.sequences_split_bars([sequence], 0)[0]
-
-    if quantise:
-        for bar in bars:
-            bar.sequence.quantise_and_normalise()
-
-    sequence = Sequence()
-    sequence.concatenate([bar.sequence for bar in bars])
-
-    tokens_bars = []
-
-    for i, bar in enumerate(bars):
-        bar_tokens = tokeniser.tokenise(bar.sequence)
-        tokens_bars.append(bar_tokens)
-
-    tokens = []
-
-    for single_bar_tokens in tokens_bars:
-        tokens.extend(single_bar_tokens)
-
-    if not detokenise:
-        return
-
-    sequence_roundtrip = tokeniser.detokenise(tokens)
-
-    if quantise:
-        sequence_roundtrip.quantise_and_normalise()
-
-    assert sequence == sequence_roundtrip
-
-    return tokens, sequence, sequence_roundtrip, tokens_bars
